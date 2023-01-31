3d4477137482cf8419c21abff21f1d52bed9c5c5
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-01-31 03:36:01 +0300

    apdate Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 47
+//Задайте двумерный массив размером m×n,
+//заполненный случайными вещественными числами.
+//m = 3, n = 4.
+
+// 1. Создание маcсива
+double[,] CreateArray(int m, int n)
+{
+  Random rnd = new Random();
+
+  double[,] myArray = new double[m, n];
+
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+    {
+      myArray[i, j] = Math.Round(rnd.NextDouble(), 2);
+    }
+
+  return myArray;
+}
+Console.WriteLine("Введите m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+var myArray = CreateArray(m, n);
+
+for (int i = 0; i < m; i++)
+{
+  Console.WriteLine();
+  for (int j = 0; j < n; j++)
+  {
+    Console.Write($"{myArray[i, j]}   ");
+  }
+}
+
+Console.ReadLine();
